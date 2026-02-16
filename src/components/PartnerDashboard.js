e9e803c58d6940e4e604d614beb08667ead48073
bappidas-git <mail4bappidas@gmail.com>
2026-02-16 16:44:59 +0530

    Updated Title

--- src/components/PartnerDashboard.js
+++ src/components/PartnerDashboard.js
@@ -95,7 +95,8 @@ const PartnerDashboard = ({ onLogout }) => {
           <img src={Logo} alt="Living Fulfilled" className="header-logo" />
         </div>
 
-        <h1 className="header-title">B2B Partner Portal</h1>
+        <h1 className="header-title">Living Fulfilled - Fulfillment Hub</h1>
+        <h5>Your One Stop Shop for all Living Fulfilled Assets</h5>
 
         <button className="logout-button-header" onClick={handleLogoutClick}>
           <Logout fontSize="small" />
